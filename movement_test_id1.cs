f17a923611875ec9933b61216947e490aef7cf3c
Nathan Konigkramer <98358837+NateKonigkramer214@users.noreply.github.com>
2023-02-25 11:42:15 +1300

    Changes

--- movement_test_id1.cs
+++ movement_test_id1.cs
@@ -8,28 +8,21 @@ public class Movement : MonoBehaviour
 {
     private Rigidbody rb;
     private float timePlayed;
-    void Start()
-    {
+    void Start(){
         Elements();
         rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
     }
     public float speed = 10.0f;
-    void Update()
-    {
+    void Update(){
         
         timePlayed += Time.deltaTime;
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
         transform.position = transform.position + new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime;
-
-        if(Input.GetAxis("Horizontal") == true){
-            
-        }
     }
 
-    private void Elements()
-    {
+    private void Elements(){
         rb = GetComponent<Rigidbody>();
     }
 }
